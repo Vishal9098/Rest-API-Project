3b483f5e167e97cb85617543f448bea3c54729d7
Vishal9098 <vishalsilawat6@gmail.com>
2026-01-08 00:17:46 +0530

    model updates

--- app/serializers.py
+++ app/serializers.py
@@ -1,30 +1,17 @@
 from rest_framework import serializers
 from .models import Employee
+from django.contrib.auth.models import User
 
-class EmployeeSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=30)
-    phone = serializers.CharField(max_length=30)
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Employee
+        fields='__all__'
+  
+    
 
-    def create(self , validated_data):
-        print("Create Method Colled..")
-        return Employee.objects.create(**validated_data)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields='__all__'
    
-
-    def update(self, employee, validated_data):
-        for attr, value in validated_data.items():
-            setattr(employee, attr, value)
-        employee.save()
-        return employee
-
-    
-
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=30)
-    first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=30)
-    
     
